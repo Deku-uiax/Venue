2da7556de06f381054007572f0d334467118bb1a
Deku-uiax <uzzallpaudel591@gmail.com>
2025-08-05 11:52:43 +0545

    Updated features / fixed bugs

---
+++ main/testimonials.py
@@ -0,0 +1,46 @@
+testimonials = [
+    {
+        "name": "Swostika Poudel",
+        "college": "Little Angels College",
+        "address": "Hattiban, Lalitpur",
+        "image": "images/bunu.jpg",
+        "comment": """
+            I would like to thank <b>Venue Education Consultancy</b> and will
+            encourage all of you to approach them if you have want to study,
+            they will guide you the best college.
+            """,
+    },
+    {
+        "name": "Merina Chaudhary",
+        "college": "Hetuda School of Management Secondary School",
+        "address": "Huprachaur, Hetuda",
+        "image": "images/merina.jpg",
+        "comment": """
+            I was properly guided and the entire process was hassle free. My
+            sincere gratitude to <b>Venue Education Consultancy</b> for making
+            my dream come true.
+            """,
+    },
+    {
+        "name": "Sabin Sheresta",
+        "college": "Shankarapur Academy",
+        "address": "Kathmandu",
+        "image": "images/sabin.jpg",
+        "comment": """
+            I was very confused after XII. Not knowing where to take admission,
+            then <b>Venue Education Consultancy</b> help me to select the best
+            option that suits me.
+            """,
+    },
+    {
+        "name": "Sita Ram Oli",
+        "college": "Eastern Region campus",
+        "address": "Dharan",
+        "image": "images/sitaram.jpg",
+        "comment": """
+            Becoming an Engineer is my dream but fees was beyond my reach. But
+            <b>Venue Education Consultancy</b>, gave me options where I could
+            enroll myself without worry.
+            """,
+    }
+]
